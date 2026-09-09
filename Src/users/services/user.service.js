36d2a42875d6e7723b18b4ea0cb1bc70da10c0c8
prkdeinze <amominkhan8@gmail.com>
2026-09-09 20:17:19 +0200

    Update user.service.js

--- Src/users/services/user.service.js
+++ Src/users/services/user.service.js
@@ -1,1 +1,22 @@
 
+const getUserProfile = async (userId) => {
+  return {
+    success: true,
+    message: "User profile service is working",
+    userId
+  };
+};
+
+const updateUserProfile = async (userId, userData) => {
+  return {
+    success: true,
+    message: "User profile update service is working",
+    userId,
+    data: userData
+  };
+};
+
+module.exports = {
+  getUserProfile,
+  updateUserProfile
+};
