e93efae726e6a31e5ffd40e17c28478592afe20a
prkdeinze <amominkhan8@gmail.com>
2026-09-09 20:08:47 +0200

    Update user.controller.js

--- Src/users/controllers/user.controller.js
+++ Src/users/controllers/user.controller.js
@@ -1,1 +1,19 @@
 
+const getProfile = async (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "User profile endpoint is working"
+  });
+};
+
+const updateProfile = async (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "User profile update endpoint is working"
+  });
+};
+
+module.exports = {
+  getProfile,
+  updateProfile
+};
