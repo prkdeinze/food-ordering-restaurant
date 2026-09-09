dd7734227a3d7bcd1e46a3da628e8f2cd3b6a85f
prkdeinze <amominkhan8@gmail.com>
2026-09-09 18:47:54 +0200

    Update auth.controller.js

--- Src/auth/Controllers/auth.controller.js
+++ Src/auth/Controllers/auth.controller.js
@@ -1,1 +1,18 @@
+const register = async (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Register endpoint is working"
+  });
+};
 
+const login = async (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Login endpoint is working"
+  });
+};
+
+module.exports = {
+  register,
+  login
+};
