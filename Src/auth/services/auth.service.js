79c03b800dcb2e22e4ac02eb7f28e7f8f013fa14
prkdeinze <amominkhan8@gmail.com>
2026-09-09 18:53:07 +0200

    Update auth.service.js

--- Src/auth/services/auth.service.js
+++ Src/auth/services/auth.service.js
@@ -1,1 +1,21 @@
 
+const registerUser = async (userData) => {
+  return {
+    success: true,
+    message: "User registration service is working",
+    data: userData
+  };
+};
+
+const loginUser = async (credentials) => {
+  return {
+    success: true,
+    message: "User login service is working",
+    data: credentials
+  };
+};
+
+module.exports = {
+  registerUser,
+  loginUser
+};
