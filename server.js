d4b846dd4948ea30f1f45e41b991bc875af0b519
prkdeinze <amominkhan8@gmail.com>
2026-09-09 20:22:40 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
 
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Food ordering server is running"
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
