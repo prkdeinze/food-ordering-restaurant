ca3ae9ff2c24a770b22361a42eb2c984f29da7f2
prkdeinze <amominkhan8@gmail.com>
2026-09-10 14:55:04 +0200

    Update user.routes.js

--- Src/users/routes/user.routes.js
+++ Src/users/routes/user.routes.js
@@ -1,13 +1,69 @@
-
 const express = require("express");
+
 const router = express.Router();
 
-const {
-  getProfile,
-  updateProfile
-} = require("../controllers/user.controller");
+const userController = require("../controllers/user.controller");
 
-router.get("/profile", getProfile);
-router.put("/profile", updateProfile);
+const {
+  validateUser,
+  validateEmail,
+  validateRole,
+  validateUserId
+} = require("../middleware/user.middleware");
+
+router.post(
+  "/",
+  validateUser,
+  validateEmail,
+  userController.createUser
+);
+
+router.get(
+  "/",
+  userController.getAllUsers
+);
+
+router.get(
+  "/:id",
+  validateUserId,
+  userController.getUserById
+);
+
+router.put(
+  "/:id",
+  validateUserId,
+  validateEmail,
+  userController.updateUser
+);
+
+router.delete(
+  "/:id",
+  validateUserId,
+  userController.deleteUser
+);
+
+router.patch(
+  "/:id/status",
+  validateUserId,
+  userController.changeUserStatus
+);
+
+router.patch(
+  "/:id/role",
+  validateUserId,
+  userController.changeUserRole
+);
+
+router.patch(
+  "/:id/verify-email",
+  validateUserId,
+  userController.verifyUserEmail
+);
+
+router.patch(
+  "/:id/verify-phone",
+  validateUserId,
+  userController.verifyUserPhone
+);
 
 module.exports = router;
