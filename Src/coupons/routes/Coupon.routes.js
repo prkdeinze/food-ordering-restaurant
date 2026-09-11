48cf22156e86b82d2abeae7a89f07a3fd05d4da7
prkdeinze <amominkhan8@gmail.com>
2026-09-11 21:37:28 +0200

    Update Coupon.routes.js

--- Src/coupons/routes/Coupon.routes.js
+++ Src/coupons/routes/Coupon.routes.js
@@ -1,1 +1,63 @@
 
+const express = require("express");
+
+const {
+  createCoupon,
+  getAllCoupons,
+  getCouponById,
+  validateCoupon,
+  updateCoupon,
+  deleteCoupon,
+} = require("../controllers/coupon.controller");
+
+const {
+  validateCouponData,
+  validateCouponId,
+  validateCouponRequest,
+} = require("../middleware/coupon.middleware");
+
+const router = express.Router();
+
+// Create coupon
+router.post(
+  "/",
+  validateCouponData,
+  createCoupon
+);
+
+// Get all coupons
+router.get(
+  "/",
+  getAllCoupons
+);
+
+// Validate coupon at checkout
+router.post(
+  "/validate",
+  validateCouponRequest,
+  validateCoupon
+);
+
+// Get one coupon
+router.get(
+  "/:id",
+  validateCouponId,
+  getCouponById
+);
+
+// Update coupon
+router.put(
+  "/:id",
+  validateCouponId,
+  validateCouponData,
+  updateCoupon
+);
+
+// Delete coupon
+router.delete(
+  "/:id",
+  validateCouponId,
+  deleteCoupon
+);
+
+module.exports = router;
