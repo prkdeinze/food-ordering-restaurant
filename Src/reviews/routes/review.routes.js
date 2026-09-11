3ce96e21d804ccfb5890a89f654dc820f76401c8
prkdeinze <amominkhan8@gmail.com>
2026-09-11 23:14:36 +0200

    Update review.routes.js

--- Src/reviews/routes/review.routes.js
+++ Src/reviews/routes/review.routes.js
@@ -1,1 +1,77 @@
+const express = require("express");
 
+const {
+  createReview,
+  getAllReviews,
+  getReviewById,
+  getReviewsByRestaurantId,
+  getReviewsByUserId,
+  updateReview,
+  deleteReview,
+} = require("../controllers/review.controller");
+
+const {
+  validateReview,
+  validateReviewId,
+  validateRestaurantId,
+  validateUserId,
+} = require("../middleware/review.middleware");
+
+const router = express.Router();
+
+/*
+|--------------------------------------------------------------------------
+| Review Routes
+|--------------------------------------------------------------------------
+*/
+
+// Create a new review
+router.post(
+  "/",
+  validateReview,
+  createReview
+);
+
+// Get all reviews
+router.get(
+  "/",
+  getAllReviews
+);
+
+// Get reviews by restaurant ID
+router.get(
+  "/restaurant/:restaurantId",
+  validateRestaurantId,
+  getReviewsByRestaurantId
+);
+
+// Get reviews by user ID
+router.get(
+  "/user/:userId",
+  validateUserId,
+  getReviewsByUserId
+);
+
+// Get one review by ID
+router.get(
+  "/:id",
+  validateReviewId,
+  getReviewById
+);
+
+// Update review
+router.put(
+  "/:id",
+  validateReviewId,
+  validateReview,
+  updateReview
+);
+
+// Delete review
+router.delete(
+  "/:id",
+  validateReviewId,
+  deleteReview
+);
+
+module.exports = router;
